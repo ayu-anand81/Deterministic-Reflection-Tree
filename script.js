30bfacee55e3bc09388d1ba7014d493ab68b0031
Ayushi Sahu <ayushisahu632@gmail.com>
2026-09-25 19:19:28 +0530

    Refactor TREE_FILE loading based on URL parameters

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-// CHANGE THIS TO LOAD DIFFERENT TREE FILES
-const TREE_FILE = "reflection-tree.json"; // Change to "reflection-tree2.json" to use the other tree
+const TREE_FILE =
+  params.get("tree") === "2"
+    ? "reflection-tree2.json"
+    : "reflectionTree.json"; CHANGE THIS TO LOAD DIFFERENT TREE FILES
 
 let treeData = null;
 let currentNode = null;
